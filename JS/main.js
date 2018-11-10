9a220b6418487fa5528af9aa31c4c8839597c35b
amelia sayle <ameliasayle@gmail.com>
2018-11-10 11:17:25 -0700

    project

---
+++ JS/main.js
@@ -0,0 +1,210 @@
+// console.log("connected");
+
+// var testOne = "what kind of variable am I?";
+// //String
+
+// var testTwo = 256;
+// //number
+
+// var testThree = false;
+// //boolean
+
+// var testFour = 'true';
+// //String
+
+// var testFive = '128';
+// //String
+
+// var testSix = "";
+// //string
+
+// var testOne ="hello" + "there";
+// // "hellothere"
+
+// var testTwo = '100' + 28;
+// // "10028"
+
+// var testThree = true +3;
+// // 4
+
+// var testFour = 'false' + 5
+// // "false5"
+
+// var testFive = '128' - 3;
+// // 125
+
+// var testSix = "" * 9;
+// // 0
+
+// var testSeven = 'zero' - 1;
+// // NaN
+
+// var testEight = 'five' .length;
+// // 4
+
+// var testNine = 'five'[0] + 'ever';
+// // fever
+
+// var testTen = 'a' > 'b';
+// // false
+
+// var testEleven = 'number'.length < 7;
+// // true
+
+// var testTwelve = '12' ==12;
+// // true
+
+// var testThirteen = 'twelve' == 12;
+// //false
+
+// var testFourteen = '14' ===14;
+// //false
+
+// var testFifteen = 'hi there'[0] + 'open sesame'[2] + 'alohamora'[1] + 'eulalia'[4] + 'harkonnen'[4] + 'BATMAN!'[6];
+// //hello
+
+// // 7. "Hello, my name is Kingsley Pappagiorgio"
+
+// // 8.120
+
+// // 9."1010ten"
+
+// // 10. "Num4 is greater than 1"
+
+// //11
+
+// let num4 = 5;
+// let num5 = 0;
+
+// if(num4 < num5 || num4 === num5) {
+// 	console.log('boobs');
+// } else if(num5 === "0" || true) {
+// 	console.log('tits');
+// } else {
+// 	console.log('ass');
+// }
+
+// tits
+
+// 12. "else if statement ran"
+
+// // 13.0, t
+// 		1, h
+// 		2, i 
+// 		3, s 
+// 		4, ""
+// 		5, i 
+// 		6, s 
+// 		7, ""
+// 		8, a 
+// 		9, "" 
+// 		10, s 
+// 		11, t
+// 		12, r 
+// 		13, i 
+// 		14, n 
+ // 15, g
+
+
+// 14. 15
+
+
+
+// 15.
+
+// 0 "o"
+// 2 "o"
+// 6 "o"
+// 8 "o"
+
+// 16.
+// var string4 = "you're gonna need a bigger boat.";
+
+// for(var i = 0; i < string4.length; i++) {
+
+// 	if(string4[i] =="a" || string4[i] == "e" ||
+// 		string4[i] == "i" || string4[i] == "o" ||
+// 		string4[i] == "u") {
+// 		console.log(string4[i]);
+// 	};
+// };
+
+// o
+// u
+// e
+// o
+// a
+// e
+// a
+// i
+// e
+// o
+// a
+
+// 17.
+// function testStuff(a, b) {
+// 	if(a > 25 && b < 100) { 
+
+// console.log(a + " is greater than 25 and " + b + " is less than 100");
+// } else {
+// 	console.log(a + " is NOT greater than 25 or " + b + " is NOT less than 100");
+// };
+// };
+
+// testStuff(10, 101);
+// testStuff(26, 99);
+// testStuff(100, 25);
+
+// 10 is NOT greater than 25 or 101 is NOT less than 100
+// 26 is greater than 25 and 99 is less than 100
+// 100 is greater than 25 and 25 is less than 100
+
+// // 18.
+// function testStuff2(c, d) {
+	
+// 	if(c == 25) {
+// return c + " is equal to 25";
+
+// } else if (d <= 100) {
+// 	return d + " is less than or equal to 100";
+
+// } else {
+// 	return "Neither if nor else if statement ran";
+
+// };
+// };
+
+// testStuff2(26, 101);
+// testStuff2(25, 99);
+// testStuff2(102, 24);
+
+// "24 is less than or equal to 100"
+
+
+// 19.
+
+// function countVowels(someString) {
+
+// 	var vowels = 0;
+	
+// 	for(var i = 0; i <someString.length; i++) {
+
+// 	if(someString[i] == "a" || someString[i] == "e" 
+// 		|| someString[i] == "i" || someString[i] 
+// 		== "o" || someString[i] == "u") {
+
+// 		vowels++;
+
+// };
+// };
+
+// 	return vowels;
+
+// };
+// countVowels("I am the very model of a modern major general");
+
+// countVowels("Exeunt pursued by a bear");
+
+// countVowels("o1234567891o");
+
+// // 8
